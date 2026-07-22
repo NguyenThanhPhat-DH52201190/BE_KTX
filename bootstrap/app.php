007a064910d468354a3a218943014d540dc72255
camtu-12 <nguyentu12082004@gmail.com>
2026-07-22 21:52:17 +0700

    fix: chuyển HandleCors sang prepend để middleware CORS chạy trước, tránh thiếu header Access-Control-Allow-Origin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
 
         // thêm dòng này
-        $middleware->append(HandleCors::class);
+        $middleware->prepend(HandleCors::class);
 
         $middleware->api(append: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
